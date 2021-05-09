e987a2f36d4abce5cabd97afd077f00599d2840a
Hector <hectoremil21@hotmail.com>
2021-05-08 21:30:43 -0400

    First pass at square function program.

---
+++ square.s
@@ -0,0 +1,26 @@
+.section .data
+.section .text
+
+.gobl _start
+_start:
+pushl $5     # This'll be our argument
+call square
+
+addl $4, %esp
+
+movl %eax, %ebx
+
+movl $1, %eax
+int $0x80
+
+.type square, @function
+square:
+pushl %ebp
+movl %esp, %ebp
+movl 8(%ebp), %eax
+
+imull 8(%ebp), %eax
+
+movel %ebp, %esp
+popl %ebp
+ret
