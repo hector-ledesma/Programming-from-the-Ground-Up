d52053c07a1b95d39902a59af0439046a11cfec8
Hector <hectoremil21@hotmail.com>
2021-05-11 16:04:20 -0400

    Figured out hardcoded indexing of the passed in array.

--- maximumlist.s
+++ maximumlist.s
@@ -1,30 +1,35 @@
 .section .data
-.section .text
-
-.globl _start
-_start:
-
 # First, we need the list that we'll pass.
 list1:
 .long 12,1,29,9
 
+.section .text
+.globl _start
+_start:
+
 # Then we push it into memory I guess?
-pushl list1
+pushl $list1
 # And we call our function
 call maximum
+addl $4, %esp
+
+movl %eax, %ebx
+movl $1, %eax
+int $0x80
 
 .type maximum, @function
 maximum:
 # Standard ebp and esp altering
 pushl %ebp
 movl %esp, %ebp
-movl $0, %edi   # Set our index to 0
+movl $8, %edi   # Set our index to 2
 
-# Move list into ebx?
+# Move list address into ebx?
 movl 8(%ebp), %ebx
 
-# For testing, let's move 29 into eax and return it
-movl %ebx(,$2,4), %eax
+# And then try to offset the address?
+# LETS GOOOOOOOOOOOOOOOOOOOOOOOOOO
+movl %edi(%ebx), %eax
 
 # Standard function ending
 movl %ebp, %esp
